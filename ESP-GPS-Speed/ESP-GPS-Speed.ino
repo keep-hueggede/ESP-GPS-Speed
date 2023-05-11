628fda4b6ef14df8a5a7a3ccc7ffe7bc2901c9d9
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2023-05-11 09:10:10 +0200

    array arg

--- ESP-GPS-Speed/ESP-GPS-Speed.ino
+++ ESP-GPS-Speed/ESP-GPS-Speed.ino
@@ -7,6 +7,7 @@
 #include <SD.h>
 #include <ArduinoJson.h>
 
+//Global defines
 #define CS 4
 
 //global vars
@@ -39,66 +40,119 @@ RaceSet race;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  
   Serial.begin(9600);
   while (!Serial) continue;  // wait for serial port to connect. Needed for native USB port only
 
   //init GPS (serial connection)
   SerialGPS.begin(9600);
+  while (!SerialGPS) continue;
 
   //init SD card
-  SD.begin(CS);
+  boolean sdCheck = SD.begin(CS);  
+  if(!sdCheck)  {
+    Serial.println("SD Card open failed");
+  }
   initConf(&conf);
 
   race.driver = "Robin";
-  race.raceID = String(millis());  // @TODO find a unique value that's different foreach measure
-  // strcpy(race.driver, "Robin");
-  // strcpy(race.raceID, String(millis()));
+  race.raceID = random();  // @TODO find a unique value that's different foreach measure
+  writeRaceSetJsonFile(&race, "/race" + race.raceID, true);
 }
 
 void loop() {
-  // speedPoint sp;
+  // SpeedPoint sp;
   // measureSpeed(&sp);
-  // //@TODO Write to SD card
-  // Serial.print("\nRaceID: ");
-  // Serial.print(race.raceID);
-  // Serial.print("\nDriver: ");
-  // Serial.print(race.driver);
-  // Serial.print("\nKM/H: ");
-  // Serial.print(sp.speedPoint);
-  // // Serial.print("\nMax KM/H: ");
-  // // Serial.print(race.maxSpeed);
-  // // Serial.print("\nAvg KM/H: ");
-  // // Serial.print(race.avgSpeed);
-  // Serial.print("\nPoint Count: ");
-  // Serial.print(race.iPoints);
+  // // //@TODO Write to SD card
+  // // Serial.print("\nRaceID: ");
+  // // Serial.print(race.raceID);
+  // // Serial.print("\nDriver: ");
+  // // Serial.print(race.driver);
+  // // Serial.print("\nKM/H: ");
+  // // Serial.print(sp.speedPoint);
+  // // // Serial.print("\nMax KM/H: ");
+  // // // Serial.print(race.maxSpeed);
+  // // // Serial.print("\nAvg KM/H: ");
+  // // // Serial.print(race.avgSpeed);
+  // // Serial.print("\nPoint Count: ");
+  // // Serial.print(race.iPoints);
 
-  delay(conf.samplingDelay);
-  digitalWrite(LED_BUILTIN, LOW);
+  // delay(conf.samplingDelay);
+  // digitalWrite(LED_BUILTIN, LOW);
 }
 
-void initConf(Config* conf) {
-  //init config file
-  StaticJsonDocument<256> confJson;
-  File file = SD.open("/config.rob", FILE_READ);
-  if(!file){
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println(F("Err read file"));
-  }
-  // while(file.available()){
-  //       Serial.write(file.read());
-  //   }
+void writeRaceSetJsonFile(RaceSet *race, String path, boolean overwrite){
+  StaticJsonDocument<256> raceJson;
+  //TODO Map
 
-  DeserializationError error = deserializeJson(confJson, file); //"{'drivers': ['Robin', 'Florian', 'Nils', 'Tim'], 'samplingHz': 1}");
+  writeJsonFile(path, &raceJson, overwrite);
+}
+
+//write to file overwrite: true -> overwrite, false -> append
+void writeJsonFile(String path, JsonDocument* fileJson, boolean overwrite) {
+  // StaticJsonDocument<size> fileJson;
+  File file;
+  switch (overwrite) {
+    case true:
+      file = SD.open(path, FILE_WRITE);
+      break;
+    case false:
+      file = SD.open(path, O_APPEND);
+      break;
+  }
+
+
+  if (!file) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println(F("\nErr read file"));
+  }
+
+  DeserializationError error = serializeJson(*fileJson, file);  //"{'drivers': ['Robin', 'Florian', 'Nils', 'Tim'], 'samplingHz': 1}");
 
   if (error) {
     digitalWrite(LED_BUILTIN, HIGH);
     Serial.println(F("Err file to json"));
   }
 
+  file.close();
+}
+
+// read & deserialize json file
+void readJsonFile(char* path, JsonDocument* fileJson) {
+  Serial.print("Read file: ");
+  Serial.print(path);
+  Serial.print(" - existing: ");
+  Serial.println(SD.exists(path));
+
+
+  File file = SD.open(path, FILE_READ);
+
+  if (!file) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Err read file");
+  }
+
+  DeserializationError error = deserializeJson(*fileJson, file);  //"{'drivers': ['Robin', 'Florian', 'Nils', 'Tim'], 'samplingHz': 1}");
+
+  if (error) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Err file to json");
+  }
+  file.close();
+}
+
+//read config
+void initConf(Config* conf) {
+  //init config file
+  StaticJsonDocument<256> confJson;
+  char fName[] = "/config.rob";
+  readJsonFile(fName, &confJson);
+
+
   conf->samplingDelay = confJson["samplingHz"];
   conf->samplingDelay = (1000 / conf->samplingDelay) | 2000;
-  Serial.print("\nDelay: ");
-  Serial.print(conf->samplingDelay);
+  Serial.print("Delay: ");
+  Serial.println(conf->samplingDelay);
 
   JsonArray arr = (confJson["drivers"]).as<JsonArray>();
 
@@ -106,14 +160,13 @@ void initConf(Config* conf) {
     int siz = sizeof(confJson["drivers"][i]) + 1;  //+1 because of termination (i guess)
     if (siz > 0) strlcpy(conf->drivers[i], confJson["drivers"][i], siz);
 
-    Serial.print("\ndrivers: ");
-    Serial.print(conf->drivers[i]);
+    //   Serial.print("\ndrivers: ");
+    //   Serial.print(conf->drivers[i]);
 
-    Serial.print(" - ");
-    Serial.print(siz);
+    //   Serial.print(" - ");
+    //   Serial.print(siz);
+    // }
   }
-
-  file.close();
 }
 
 void measureSpeed(SpeedPoint* speedo) {
