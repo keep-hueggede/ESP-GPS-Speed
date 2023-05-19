f422f469fda6658937359fc83ab2b827e44bffba
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2023-05-19 13:00:13 +0200

    taster working and entprellt

--- ESP-GPS-Speed/ESP-GPS-Speed.ino
+++ ESP-GPS-Speed/ESP-GPS-Speed.ino
@@ -1,13 +1,11 @@
 #include <SoftwareSerial.h>
 #include "TinyGPS++.h"
 #include <time.h>
-#include <Wire.h>  // Wire Bibliothek einbinden
+// #include <Wire.h>  // Wire Bibliothek einbinden
 #include <LiquidCrystal_I2C.h>
-
-// include the SD library:
-#include <SPI.h>
 #include <SD.h>
 #include <ArduinoJson.h>
+#include <Bounce2.h>
 
 //Global defines
 #define CS 4
@@ -22,7 +20,7 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 typedef struct {
   int samplingDelay;
   int aggregationCount;
-  char drivers[3][10];
+  char drivers[3][3];
 } Config;
 
 typedef struct {
@@ -42,35 +40,33 @@ typedef struct {
 } RaceSet;
 
 //global vars
-volatile int sStat2 = 0;  //Interrupt status Pin2
-volatile int sStat3 = 0;  //Interrupt status Pin3
+// volatile int sStat2 = 0;  //Interrupt status Pin2
+// volatile int sStat3 = 0;  //Interrupt status Pin3
 Config conf;
 RaceSet race;
 int currentDriver;
 boolean raceRunning = false;
 
+Bounce DSelect = Bounce();
+Bounce RaceStart = Bounce();
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(DSELECT, INPUT);
-  pinMode(RACESTART, INPUT);
+  pinMode(DSELECT, INPUT_PULLUP);
+  pinMode(RACESTART, INPUT_PULLUP);
+
+  DSelect.attach(DSELECT);
+  DSelect.interval(5);
+  RaceStart.attach(RACESTART);
+  RaceStart.interval(5);
 
   Serial.begin(115200);
-  while (!Serial) continue;  // wait for serial port to connect. Needed for native USB port only
+  // while (!Serial) continue;  // wait for serial port to connect. Needed for native USB port only
   Serial.println("---");
 
   //init GPS (serial connection)
   SerialGPS.begin(9600);
-  while (!SerialGPS) continue;
 
-  //define interrupts
-  // attachInterrupt(0, switchDriver, CHANGE);
-  // attachInterrupt(1, startEndRace, CHANGE);
-
-  //init SD card
-  boolean sdCheck = SD.begin(CS);
-  if (!sdCheck) {
-    Serial.println("SD Card open failed");
-  }
   initConf();
   currentDriver = 0;
 
@@ -82,12 +78,13 @@ void setup() {
 }
 
 void loop() {
+  // sStat2 = digitalRead(DSELECT);
+  // sStat3 = digitalRead()
+
   if (raceRunning) {
     SpeedPoint sp;
     measureSpeed(&sp);
-
-
-
+    printLCD(sp.speedPoint);
 
     // //@TODO Write to SD card
     // Serial.print("\nRaceID: ");
@@ -106,6 +103,11 @@ void loop() {
     // // delay(conf.samplingDelay);
     // digitalWrite(LED_BUILTIN, LOW);
   }
+
+  RaceStart.update();
+  if (RaceStart.fallingEdge()) startEndRace();
+  DSelect.update();
+  if (DSelect.fallingEdge()) switchDriver();
 }
 
 void writeRaceSetJsonFile(RaceSet* race, boolean overwrite) {
@@ -144,6 +146,12 @@ void writeSpeedpointToRaceFile(SpeedPoint* speedo) {
 //write to file overwrite: true -> overwrite, false -> append
 void writeJsonFile(char* path, JsonDocument* fileJson, boolean overwrite) {
   // StaticJsonDocument<size> fileJson;
+  //init SD card
+  boolean sdCheck = SD.begin(CS);
+  if (!sdCheck) {
+    Serial.println("SD Card open failed");
+  }
+
   File file;
   switch (overwrite) {
     case true:
@@ -168,52 +176,57 @@ void writeJsonFile(char* path, JsonDocument* fileJson, boolean overwrite) {
   }
 
   file.close();
+  SD.end();
 }
 
 // read & deserialize json file
-void readJsonFile(char* path, JsonDocument* fileJson) {
-  Serial.print("Read file: ");
-  Serial.print(path);
-  // Serial.print(" - existing: ");
-  // Serial.println(SD.exists(path));
+// void readJsonFile(char* path, JsonDocument* fileJson) {
+//   // Serial.print("Read file: ");
+//   // Serial.print(path);
+//   // Serial.print(" - existing: ");
+//   // Serial.println(SD.exists(path));
 
+//  //init SD card
+//   boolean sdCheck = SD.begin(CS);
+//   if (!sdCheck) {
+//     Serial.println("SD Card open failed");
+//   }
 
-  File file = SD.open(path, FILE_READ);
+//   File file = SD.open(path, FILE_READ);
 
-  if (!file) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("Err read file");
-  }
+//   if (!file) {
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     Serial.println("Err read file");
+//   }
 
-  DeserializationError error = deserializeJson(*fileJson, file);  //"{'drivers': ['Robin', 'Florian', 'Nils', 'Tim'], 'samplingHz': 1}");
+//   DeserializationError error = deserializeJson(*fileJson, file);  //"{'drivers': ['Robin', 'Florian', 'Nils', 'Tim'], 'samplingHz': 1}");
 
-  if (error) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("Err file to json");
-  }
-  file.close();
-}
+//   if (error) {
+//     digitalWrite(LED_BUILTIN, HIGH);
+//     Serial.println("Err file to json");
+//   }
+//   file.close();
+//   SD.end();
+// }
 
 //read config
 void initConf() {
-
   conf.samplingDelay = 500;
   conf.aggregationCount = 10;
   for (int i = 0; i < 3; i++) {
     switch (i) {
       case 0:
-        strcpy(conf.drivers[i], "ROBIN");
+        strcpy(conf.drivers[i], "RW");
         break;
       case 1:
-        strcpy(conf.drivers[i], "GNADENLOS");
+        strcpy(conf.drivers[i], "FH");
         break;
       case 2:
-        strcpy(conf.drivers[i], "KUHNEBOLLE");
+        strcpy(conf.drivers[i], "NK");
         break;
     }
   }
 }
-
 
 
 void measureSpeed(SpeedPoint* speedo) {
@@ -235,24 +248,24 @@ void measureSpeed(SpeedPoint* speedo) {
   }
 }
 
-double findMax(double inputArray[], int* size) {
-  // assert(inputArray && size);
-  double max = inputArray[0];
-  for (int i = 1; i < size; i++) {
-    if (inputArray[i] > max) {
-      max = inputArray[i];
-    }
-  }
-  return max;
-}
+// double findMax(double inputArray[], int* size) {
+//   // assert(inputArray && size);
+//   double max = inputArray[0];
+//   for (int i = 1; i < size; i++) {
+//     if (inputArray[i] > max) {
+//       max = inputArray[i];
+//     }
+//   }
+//   return max;
+// }
 
-double calcAvg(double inputArray[], int* size) {
-  double sum = 0;
-  for (int i = 0; i < size; i++) {
-    sum = sum + inputArray[i];
-  }
-  return sum / *size;
-}
+// double calcAvg(double inputArray[], int* size) {
+//   double sum = 0;
+//   for (int i = 0; i < size; i++) {
+//     sum = sum + inputArray[i];
+//   }
+//   return sum / *size;
+// }
 
 void buildGPSDateTime(char* string) {
   TinyGPSDate d = gps.date;
@@ -261,21 +274,27 @@ void buildGPSDateTime(char* string) {
   sprintf(string, "%04d-%02d-%02dT%02d:%02d:%02d.000Z", d.year(), d.month(), d.day(), t.hour(), t.minute(), t.second());
 }
 
-void printLCD(double speed){
-    lcd.clear();    
-    lcd.setCursor(0, 0);        
-    lcd.print("Wheel:");    
-    lcd.print(conf.drivers[1]);
+void printLCD(double speed) {
+  Serial.print("Print LCD: ");
+  Serial.print(conf.drivers[currentDriver]);
+  Serial.print(" - race: ");
+  Serial.println(raceRunning);
 
-    lcd.setCursor(0, 1);
-    if(raceRunning){              
-      lcd.print("Speed: ");
-      lcd.print(speed);
-      lcd.print("km/h");
-    }else{
-      lcd.print("Press Start");
-    }
 
+  lcd.clear();
+  // delay(5000);
+  lcd.setCursor(0, 0);
+  lcd.print("Wheel:");
+  lcd.print(conf.drivers[currentDriver]);
+
+  lcd.setCursor(0, 1);
+  if (raceRunning) {
+    lcd.print("Speed: ");
+    lcd.print(speed);
+    lcd.print("km/h");
+  } else {
+    lcd.print("Press Start");
+  }
 }
 
 /**********************
@@ -285,25 +304,34 @@ void startEndRace() {
   if (!raceRunning) {
     //TODO: new Raceset --> driver, starttime
     //TODO: trigger via interrupt
+
     race.driver = conf.drivers[currentDriver];
     race.raceID = random(0, 1000000);
     buildGPSDateTime(race.startTime);
-    raceRunning = true;    
+    raceRunning = true;
+    // Serial.print("Start race");
   } else {
     //TODO: finish Raceset --> endtime, calc's, write to file
     //TODO: trigger via interrupt
     raceRunning = false;
     buildGPSDateTime(race.startTime);
     writeRaceSetJsonFile(&race, true);
+    // Serial.print("End race");
+
+    SerialGPS.end();
   }
+  printLCD(0.0);
 }
 
 void switchDriver() {
-  if(raceRunning) return;
+  // Serial.print("Switch driver ");
+  if (raceRunning) return;
 
   if (currentDriver == 2) {
     currentDriver = 0;
   } else {
     currentDriver++;
   }
+  // Serial.println(conf.drivers[currentDriver]);
+  printLCD(0.0);
 }
